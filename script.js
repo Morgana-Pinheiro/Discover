f4074294f73db1f7393faf60dafed47d361b68d4
Morgana <pinheiromorg@gmail.com>
2024-07-15 12:24:16 -0300

    mudanças no código

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  //if(html.classList.contains (light)) {
-  // html.classList.remove(light)
-  //} else {
-  //  html.classList.add(light)
-  //}
+
+  // pegar a tag img
   const img = document.querySelector("#profile img")
 
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "/pessoal/eu.jpg")
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./pessoal/eu-virado.jpg")
   } else {
-    img.setAttribute("src", "/pessoal/eu-virado.jpg")
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./pessoal/eu.jpg")
   }
 }
